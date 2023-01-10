238f3441057d026443fe093cef7a410d431c8437
Niraj <niraj.webtech@gmail.com>
2023-01-10 20:44:33 +0530

    Starting the GIT

---
+++ src/main/java/com/niraj/SprEntityApplication.java
@@ -0,0 +1,14 @@
+package com.niraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprEntityApplication.class, args);
+		System.out.println("Project [ spr-entity ] for JPA Entity Relation" );
+	}
+
+}
