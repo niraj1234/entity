11ebdf13ab85134a58799ac7fe5cf6acdca2a1e1
Niraj <niraj.webtech@gmail.com>
2023-01-11 03:53:17 +0530

    Adding Dockerfile

--- src/test/java/com/niraj/SprEntityApplicationTests.java
+++ src/test/java/com/niraj/SprEntityApplicationTests.java
@@ -1,12 +1,7 @@
 package com.niraj;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SprEntityApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
