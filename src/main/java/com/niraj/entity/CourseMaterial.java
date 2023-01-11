2d552663a48a722052b81f778e6195de18281f56
Niraj <niraj.webtech@gmail.com>
2023-01-11 13:12:05 +0530

    Adding course material

---
+++ src/main/java/com/niraj/entity/CourseMaterial.java
@@ -0,0 +1,26 @@
+package com.niraj.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseMaterial {
+
+	@Id
+	@SequenceGenerator(name = "course-material-name" , sequenceName="course-material-seq-name" , allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "course-material-gen-tbl")
+	private Long courseMaterialId;
+	
+	private String url;
+	
+}
