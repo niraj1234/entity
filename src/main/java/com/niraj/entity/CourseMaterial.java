5e35eeae5bafab5e55db861f0472706b770c1dd2
Niraj <niraj.webtech@gmail.com>
2023-01-11 17:30:31 +0530

    Course Material Bug Resolved

--- src/main/java/com/niraj/entity/CourseMaterial.java
+++ src/main/java/com/niraj/entity/CourseMaterial.java
@@ -17,8 +17,8 @@ import lombok.NoArgsConstructor;
 public class CourseMaterial {
 
 	@Id
-	@SequenceGenerator(name = "course-material-name" , sequenceName="course-material-gen-tbl" , allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "course-material-gen-tbl")
+	@SequenceGenerator( name = "course_material_sequence" , sequenceName = "course_material_sequence" , allocationSize = 1)
+	@GeneratedValue( strategy = GenerationType.SEQUENCE , generator = "course_material_sequence")
 	private Long courseMaterialId;
 	
 	private String url;
